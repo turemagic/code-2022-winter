ad5874f9a0430157e89079f2e238cf8e4056ef11
初嵩剑 <chusongjian@outlook.com>
2022-01-27 19:28:39 +0800

    Add files via upload
    2022-1.27.test

--- test.c
+++ test.c
@@ -1,45 +1,82 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-
+#define _CRT_SECURE_NO_WARNINGS
 #include"game.h"
-void game() {
-	char arr[H][L] = { 0 };
-	char arr1[H][L] = { 0 };
-	int n;
-	printf("扫雷游戏即将开始，敬请期待\n");
-	board(arr, H, L);
-	playboard(arr, H, L);
-	n = 1;
-	while (n)
+void menu()
+{
+	printf("**********************************************\n");
+	printf("****************  1.play  ********************\n");
+	printf("****************  0.exit  ********************\n");
+	printf("**********************************************\n");
+}
+void game()
+{
+	char ret = 0;
+	//存放下棋的数据
+	char board[ROW][COL] = { 0 };
+	//初始化棋盘为全空格
+	init_board(board, ROW, COL);
+	//打印棋盘
+	display_board(board, ROW, COL);
+	while (1)
 	{
-		play(arr, arr1, H, L);
-		n = win(arr, H, L);
+		//玩家下棋
+		player_move(board, ROW, COL);
+		display_board(board, ROW, COL);
+		//判断输赢
+		ret = is_win(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//电脑下棋
+		computer_move(board, ROW, COL);
+		display_board(board, ROW, COL);
+		//判断输赢
+		ret = is_win(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
 	}
+	if (ret == '*')
+	{
+		printf("玩家赢了\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢了\n");
+	}
+	else if (ret == 'Q')
+	{
+		printf("平局\n");
+	}
+
+		
 }
-void menu() {
-	printf("**********************\n");
-	printf("****1.play 0.exit*****\n");
-	printf("**********************\n");
-}
-int main() {
-	int n;
-	do {
+//玩家赢--'*'
+//电脑赢--'#'
+//平局--'Q'
+//继续--'C'
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
 		menu();
-	a:
-		printf("请选择：");
-		scanf("%d", &n);
-		switch (n)
+		printf("请输入：");
+		scanf("%d", &input);
+		switch (input)
 		{
 		case 1:
 			game();
 			break;
 		case 0:
-			printf("退出程序\n");
+			printf("退出游戏\n");
 			break;
 		default:
-			printf("输出错误，请重新输入\n");
-			goto a;
+			printf("输入错误，请重新输入\n");
+			break;
 		}
-	} while (n);
+	} while (input);
 	return 0;
 }
